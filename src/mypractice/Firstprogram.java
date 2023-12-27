35d9cae3b404bb6fb8b1a7c6b8ed97ec93103aec
Winchester5@% <lsraj123@gmail.com>
2023-12-26 21:18:46 -0500

    commitsum of numbers in src

---
+++ src/mypractice/Firstprogram.java
@@ -0,0 +1,15 @@
+package mypractice;
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		System.out.println("Helloworld");
+		int a=20;
+		int b=10;
+		int c=a+b;
+		System.out.println(c);
+		// TODO Auto-generated method stub
+
+	}
+
+}
